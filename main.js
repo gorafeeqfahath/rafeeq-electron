c3bf81cc6fa3581cd8963028ebd9b9ec7fd864da
Fahath Mohamed <mtmfahath@gorafeeq.com>
2021-03-09 12:52:48 +0300

    Minimize to Tray added

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, screen, Tray, Menu, dialog } = require("electron");
+const path = require("path");
 
 const setupEvents = require("./installers/setupEvents");
+
+let win;
+let mainWindow;
 
 if (setupEvents.handleSquirrelEvent()) {
   // squirrel event handled and app will exit in 1000ms, so don't do anything else
@@ -8,16 +12,35 @@ if (setupEvents.handleSquirrelEvent()) {
 }
 
 function createWindow() {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+
+  win = new BrowserWindow({
+    width: width,
+    height: height,
+    center: true,
+    thickFrame: true,
     webPreferences: {
       nodeIntegration: true,
       webSecurity: false,
+      devTools: false,
     },
   });
-  win.menuBarVisible = false;
   win.loadURL("http://portal.gorafeeq.com");
+
+  let tray = null;
+  win.on("minimize", function (event) {
+    event.preventDefault();
+    win.setSkipTaskbar(true);
+    tray = createTray();
+  });
+
+  win.on("restore", function (event) {
+    win.show();
+    win.setSkipTaskbar(false);
+    tray.destroy();
+  });
+
+  return win;
 }
 
 app.whenReady().then(createWindow);
@@ -33,3 +56,29 @@ app.on("activate", () => {
     createWindow();
   }
 });
+
+function createTray() {
+  let appIcon = new Tray(path.join(__dirname, "assets/icons/win/favicon.ico"));
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: "Show",
+      click: function () {
+        win.show();
+      },
+    },
+    {
+      label: "Exit",
+      click: function () {
+        app.isQuiting = true;
+        app.quit();
+      },
+    },
+  ]);
+
+  appIcon.on("double-click", function (event) {
+    mainWindow.show();
+  });
+  appIcon.setToolTip("Rafeeq Vendor");
+  appIcon.setContextMenu(contextMenu);
+  return appIcon;
+}
